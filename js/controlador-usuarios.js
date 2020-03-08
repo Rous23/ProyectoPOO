8be7bca5248d126d8332cd5c44a7c3c5b254f48b
Rosa_Avila <rosaavila14.2015@gmail.com>
2020-03-07 22:18:32 -0600

    version1

---
+++ js/controlador-usuarios.js
@@ -0,0 +1,53 @@
+ var usuarios=[
+    {
+        empresa: [
+            {
+                nombre:'empresa1',
+                username:'coronavirus',
+                perfil: 'imagen.jpg',
+                portada: 'imagen2.jpg',
+                email: 'user@gmail.com',
+                contrasenia: 'password',
+                pais:'nombrePais',
+                direccion:'col.san jose',
+                tarjeta:'tipoTarjeta',
+                numeroTarjeta:'4567-4322-2344',
+                vencimiento:'fecha',
+                cvv:'234',
+                redes:[],
+                latitud:'3455',
+                longitud:'asdh'
+            }
+        ],
+        
+        cliente:[
+            {
+                nombre:'cliente1',
+                username:'Rous',
+                perfil: 'imagen.jpg',
+                portada: 'imagen2.jpg',
+                email: 'user@gmail.com',
+                contrasenia: 'password',
+                pais:'nombrePais',
+                direccion:'col.san jose',
+                tarjeta:[
+                    {
+                        numeroTarjeta:'4567-4322-2344',
+                        vencimiento:'fecha',
+                        cvv:'234'
+                    }
+                ],
+                redes:[],
+                genero:'F',
+                latitud:'3455',
+                longitud:'asdh'
+            }
+        ],
+        superAdministrador:[
+            {
+                nombre:'admin',
+                contrasenia:'asd.456'
+
+        }]
+    }
+]
