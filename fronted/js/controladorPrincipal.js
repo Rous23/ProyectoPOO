ea1533b668720edba503798c45c30fdbdedb73c2
Rosa_Avila <rosaavila14.2015@gmail.com>
2020-05-11 18:29:32 -0600

    avance

---
+++ fronted/js/controladorPrincipal.js
@@ -0,0 +1,331 @@
+var categorias =[
+    {
+        nombreCategoria:'Niños',
+        imgCategoria: 'img/niños.jpg',
+        descripcion: 'articulos para mascotas',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Libros',
+        imgCategoria: 'img/libros.jpg',
+        descripcion: 'Para Lectura',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Hogar',
+        imgCategoria: 'img/hogar.jpg',
+        descripcion: 'organizacion, ropa de cama, cocina, fiesta, decoracion.',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Calzado',
+        imgCategoria: 'img/calzado.jpg',
+        descripcion: 'Todo estilo de zapatos ya sea de hombre o mujer.',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Tecnologia',
+        imgCategoria: 'img/tecnologia.jpg',
+        descripcion: 'Computadoras, camaras, celulares, impresoras',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Moda mujer',
+        imgCategoria: 'img/modaMujer.jpg',
+        descripcion: 'vestidos, camisas, pantalones, chaquetas..',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Moda hombre',
+        imgCategoria: 'img/modaHombre.jpg',
+        descripcion: 'shorts, pantalones, busos, ropa formal...',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Deportes',
+        imgCategoria: 'img/deporte.jpg',
+        descripcion: '',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Salud y belleza',
+        imgCategoria: 'img/saludYbelleza.png',
+        descripcion: 'vitaminas, mascarillas, maquillaje... ',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Accesorios',
+        imgCategoria: 'img/accesorios.jpg',
+        descripcion: 'relojes, carteras, bisuteria, lentes... ',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Mascotas',
+        imgCategoria: 'img/mascotas.jpg',
+        descripcion: '',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Oficina',
+        imgCategoria: 'img/oficina.jpg',
+        descripcion: 'relojes, carteras, bisuteria, lentes... ',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/cards1.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    }
+];
+console.log(categorias);
+
+
+function generarCategoria(){
+    document.getElementById('card-categorias').innerHTML='';
+    for(let i = 0 ; i < categorias.length ; i++){
+        document.getElementById('card-categorias').innerHTML += `
+        <div class="col-lg-2 col-md-3 col-4 py-1">
+            <div onclick="mostrarProductos(${i})" class="card" style="cursor: pointer;">
+                <div>
+                    <img src= ${categorias[i].imgCategoria} class="d-block w-100 tamaño-img card-img-top">
+                </div>
+                <div class="card-footer py-3" style="background: #66397E; border-color: #66397E;color: white!important;padding-left: 0px!important;padding-right: 0px!important;">
+                    <p class="card-text text-center" style="font-size: 15px;padding: 0%!important;">${categorias[i].nombreCategoria}</p>
+                    <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+                </div>
+            </div>
+        </div>`;
+    }
+}
+generarCategoria();
+
+function mostrarProductos(indice){
+    document.getElementById('mostrarSubCategoria').innerHTML = categorias[indice].nombreCategoria;
+    document.getElementById('mostrarSubCategoria').innerHTML +='';
+    for (let i = 0 ; i < categorias[indice].productos.length ; i++){
+        console.log(categorias[indice].productos[i]);
+    }
+}
+
+function verCategorias(){
+    window.location.href = "subCategoria.html";
+}
+var inputs =[
+    {input:'email',valido:false},
+    {input:'contraseña',valido:false}
+];
+
+/**/
+
+function login(){
+    console.log('hola');
+    
+    for(let i = 0 ; i < inputs.length ; i++)
+        inputs[i].valido = validarCampoVacio(inputs[i].input);
+    
+    if(document.getElementById('email') != ''){
+        let respuesta = validarEmail(document.getElementById('email').value);
+        console.log(respuesta);
+        
+        inputs[1].valido = respuesta;
+        colorearInput('email', respuesta);
+    }       
+    let listo = false;
+    for(let i = 0 ; i < inputs.length ; i++){
+        if(inputs[i].valido){
+            listo = true;
+        }else{
+            listo = false;
+            break;
+        }
+    }
+
+    if(listo){
+        window.location.href = "categorias.html";
+    }
+}
+function validarCampoVacio(id){
+    let valorCampo = document.getElementById(id).value;
+    let respuesta;
+    if(valorCampo == ''){
+        respuesta = false;
+    }else{
+        respuesta = true;
+    }
+    colorearInput(id,respuesta);
+    return respuesta;
+}
+
+function colorearInput(id, respuesta){
+
+    if (!respuesta){
+        document.getElementById(id).classList.remove('is-valid');
+        document.getElementById(id).classList.add('is-invalid');
+    }else{ 
+        document.getElementById(id).classList.remove('is-invalid');
+        document.getElementById(id).classList.add('is-valid');
+    }
+}
+function validarEmail(email){
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
---
+++ fronted/js/controladorSubCategoria.js
@@ -0,0 +1,461 @@
+var categorias =[
+    {
+        nombreCategoria:'Niños',
+        imgCategoria: 'img/3.jpg',
+        icono:'fas fa-child',
+        descripcion: 'articulos para mascotas',
+        productos:[
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Libros',
+        imgCategoria: 'img/libros.jpg',
+        icono:'fas fa-book',
+        descripcion: 'Para Lectura',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Hogar',
+        imgCategoria: 'img/hogar.jpg',
+        icono:'fas fa-home',
+        descripcion: 'organizacion, ropa de cama, cocina, fiesta, decoracion.',
+        productos:[
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Calzado',
+        imgCategoria: 'img/5.jpg',
+        icono:'fas fa-shoe-prints',
+        descripcion: 'Todo estilo de zapatos ya sea de hombre o mujer.',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Tecnologia',
+        imgCategoria: 'img/5.jpg',
+        icono:'fas fa-atom',
+        descripcion: 'Computadoras, camaras, celulares, impresoras',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Moda mujer',
+        imgCategoria: 'img/5.jpg',
+        icono:'fas fa-female',
+        descripcion: 'vestidos, camisas, pantalones, chaquetas..',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Moda hombre',
+        imgCategoria: 'img/5.jpg',
+        icono:'fas fa-male',
+        descripcion: 'shorts, pantalones, busos, ropa formal...',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Deporte',
+        imgCategoria: 'img/5.jpg',
+        icono:'fas fa-blender',
+        descripcion: '',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Salud y belleza',
+        imgCategoria: 'img/5.jpg',
+        icono:'fas fa-heart',
+        descripcion: 'vitaminas, mascarillas, maquillaje... ',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Accesorios',
+        imgCategoria: 'img/5.jpg',
+        icono:'fas fa-swatchbook',
+        descripcion: 'relojes, carteras, bisuteria, lentes... ',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Mascotas',
+        imgCategoria: 'img/5.jpg',
+        icono:'fas fa-paw',
+        descripcion: '',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    },
+    {
+        nombreCategoria:'Oficina',
+        imgCategoria: 'img/5.jpg',
+        icono:'fas fa-briefcase',
+        descripcion: 'relojes, carteras, bisuteria, lentes... ',
+        productos:[
+            {
+                nombreProducto:'Carro',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Muñecas',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            },
+            {
+                nombreProducto:'Lego',
+                imgProducto: 'img/niños.jpg',
+                descripcion: "Lorem ipsum, dolor sit",
+                precioNormal: 49.99,
+                precioPromocion: 39.99,
+                porcentajeDescuento: '20%'
+            }
+        ]
+    }
+];
+
+function imprimirSidebar(){
+    document.getElementById('nav-sidebar').innerHTML='';
+    for(let i = 0 ; i < categorias.length ; i++){
+        document.getElementById('nav-sidebar').innerHTML += `
+        <li onclick="mostrarProductos(${i})" style="cursor:pointer;" class="nav-item">
+            <span class="nav-link componentes-sidebar" data-feather="file style="display:block"><i style="color:#67397F;" class="${categorias[i].icono}"></i>
+            <span>${categorias[i].nombreCategoria}</span></span>
+        </li>`;
+    }
+}
+imprimirSidebar();
+
+function mostrarProductos(id){
+    document.getElementById('mostrarSubCategoria').innerHTML='';
+    document.getElementById('mostrarSubCategoria').innerHTML= `
+    <div class="col-12 text-center" style="font-size: 25px;">${categorias[id].nombreCategoria}</div>
+    `;
+    for (let i = 0 ; i < categorias[id].productos.length ; i++){
+        console.log(categorias[id].productos[i]);
+        document.getElementById('mostrarSubCategoria').innerHTML += `
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12 py-1">
+            <div class="card shadow-sm">
+                <img src="${categorias[id].productos[i].imgProducto}" class="card-img-top" style="height:210px;">
+                <div class="card-body">
+                    <h5 class="card-title">${categorias[id].productos[i].nombreProducto}</h5>
+                    <p class="card-text">${categorias[id].productos[i].precioNormal +"  "+ categorias[id].productos[i].precioPromocion}</p>
+                </div>
+                <div class="card-footer">
+                    <p class="card-text">${categorias[id].productos[i].porcentajeDescuento}</p>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+}
+
+/*Validacion*/ 
+var inputs =[
+    {input:'email',valido:false},
+    {input:'contraseña',valido:false}
+];
+
+/**/
+
+function login(){
+    console.log('hola');
+    
+    for(let i = 0 ; i < inputs.length ; i++)
+        inputs[i].valido = validarCampoVacio(inputs[i].input);
+    
+    if(document.getElementById('email') != ''){
+        let respuesta = validarEmail(document.getElementById('email').value);
+        console.log(respuesta);
+        
+        inputs[1].valido = respuesta;
+        colorearInput('email', respuesta);
+    }       
+    let listo = false;
+    for(let i = 0 ; i < inputs.length ; i++){
+        if(inputs[i].valido){
+            listo = true;
+        }else{
+            listo = false;
+            break;
+        }
+    }
+
+    if(listo){
+        window.location.href = "categorias.html";
+    }
+}
+function validarCampoVacio(id){
+    let valorCampo = document.getElementById(id).value;
+    let respuesta;
+    if(valorCampo == ''){
+        respuesta = false;
+    }else{
+        respuesta = true;
+    }
+    colorearInput(id,respuesta);
+    return respuesta;
+}
+
+function colorearInput(id, respuesta){
+
+    if (!respuesta){
+        document.getElementById(id).classList.remove('is-valid');
+        document.getElementById(id).classList.add('is-invalid');
+    }else{ 
+        document.getElementById(id).classList.remove('is-invalid');
+        document.getElementById(id).classList.add('is-valid');
+    }
+}
+function validarEmail(email){
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
